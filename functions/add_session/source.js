19ad84ac5aafc732664fcd2f6a0818906ba80da9
JamesOsgood <james.osgood@gmail.com>
2020-08-18 16:03:50 +0100

    Change params

--- functions/add_session/source.js
+++ functions/add_session/source.js
@@ -1,7 +1,7 @@
 
-exports = function(session_date, session_type) 
+exports = function(session_start, session_end, session_type) 
 {
   const mongodb = context.services.get("mongodb-atlas");
   const sessions = mongodb.db("artheads_sessions").collection("sessions");
-  return sessions.insertOne({'session_date' : session_date, 'session_type' : session_type})
+  return sessions.insertOne({'session_start' : session_start, 'session_end' : session_end, 'session_type' : session_type})
 };
