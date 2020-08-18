ed3bf1a8a2862368c8e94a4f2f6555b6dadd06bb
JamesOsgood <james.osgood@gmail.com>
2020-08-18 16:25:39 +0100

    Return session id

--- functions/add_session/source.js
+++ functions/add_session/source.js
@@ -1,7 +1,9 @@
 
-exports = function(session_start, session_end, session_type) 
+exports = async function(session_start, session_end, session_type) 
 {
-  const mongodb = context.services.get("mongodb-atlas");
-  const sessions = mongodb.db("artheads_sessions").collection("sessions");
-  return sessions.insertOne({'session_start' : session_start, 'session_end' : session_end, 'session_type' : session_type})
+  const mongodb = context.services.get("mongodb-atlas")
+  const sessions = mongodb.db("artheads_sessions").collection("sessions")
+  var res = await sessions.insertOne({'session_start' : session_start, 'session_end' : session_end, 'session_type' : session_type})
+  var ret = { 'session_id' : res.response['_id'] }
+  return ret
 };
