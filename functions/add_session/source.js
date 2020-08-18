7df3352da1156d4046b6fa3917cecdd9cf15fca5
JamesOsgood <james.osgood@gmail.com>
2020-08-18 16:27:12 +0100

    Update

--- functions/add_session/source.js
+++ functions/add_session/source.js
@@ -4,6 +4,6 @@ exports = async function(session_start, session_end, session_type)
   const mongodb = context.services.get("mongodb-atlas")
   const sessions = mongodb.db("artheads_sessions").collection("sessions")
   var res = await sessions.insertOne({'session_start' : session_start, 'session_end' : session_end, 'session_type' : session_type})
-  var ret = { 'session_id' : res.response['_id'] }
+  var ret = { 'session_id' : res['_id'] }
   return ret
 };
