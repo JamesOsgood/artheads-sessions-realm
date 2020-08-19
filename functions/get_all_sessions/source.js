54c8eaffd3e5eca05d382ff1458bb2b87dcf3a3a
JamesOsgood <james.osgood@gmail.com>
2020-08-19 09:49:45 +0100

    Add function to get all sessions

---
+++ functions/get_all_sessions/source.js
@@ -0,0 +1,8 @@
+
+exports = function() 
+{
+  const mongodb = context.services.get("mongodb-atlas");
+  const sessions = mongodb.db("artheads_sessions").collection("sessions");
+  var filter = {}
+  return sessions.find(filter).limit(count)
+};
